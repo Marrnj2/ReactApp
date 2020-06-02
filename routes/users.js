8722c3d1220598f0a62688fa859842efbb9327a3
marrnj2 <marrnj2@student.op.ac.nz>
2020-06-02 19:28:55 +1200

    Like button added to index

---
+++ routes/users.js
@@ -0,0 +1,7 @@
+var express = require('express');
+var router = express.Router();
+const userController = require('../controllers/users');
+
+router.get('/', userController.index);
+
+module.exports = router;
